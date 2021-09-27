dfd0ea19e4b3aff96180abda556f63a9decad35a
vanghoa <baoanh1buinguyen@gmail.com>
2021-09-28 03:00:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-//import { PLYLoader } from './node_modules/three/examples/jsm/loaders/PLYLoader.js';
-//import {OrbitControls} from '../node_modules/three/examples/js/controls/OrbitControls.js';
-//import * as THREE from '../node_modules/three/build/three.js';
-//import {DragControls} from '../node_modules/three/examples/js/controls/DragControls.js';
+/*  import { PLYLoader } from './node_modules/three/examples/jsm/loaders/PLYLoader.js';
+    import {OrbitControls} from '../node_modules/three/examples/js/controls/OrbitControls.js';
+    import * as THREE from '../node_modules/three/build/three.js';
+    import {DragControls} from '../node_modules/three/examples/js/controls/DragControls.js';    */
 import * as THREE from 'https://cdn.skypack.dev/three@0.132.2/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js';
 import { DragControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/DragControls.js';
 import { PLYLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/PLYLoader.js';
+//import * as dat from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/libs/dat.gui.module.js';
 
+let objectionxoay = [];
+let objectiondrag = [];
+let soanchor = 5;
+let anchortong = new THREE.Object3D();
 
-//Loading
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
 // Board Kiem tra
 //const gui = new dat.GUI()
@@ -18,7 +26,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-//scene.background = 0xdddddd;
+scene.background = 0xdddddd;
 
 //environment
 scene.background = new THREE.Color(0xcbf4ba);
@@ -26,14 +34,77 @@ scene.background = new THREE.Color(0xcbf4ba);
 // Objects
 const geometry = new THREE.BoxGeometry(.5, .5, .5);
 
+/**
+ * Camera
+ */
+// Base camera
+let aspect = sizes.width / sizes.height;
+const frustum = 10;
+const camerakhoangcach = 30;
+const camera = new THREE.OrthographicCamera(-aspect * frustum, aspect * frustum, frustum, -frustum, 1, 1000)
+
+// Controls Orbit
+const controls = new OrbitControls(camera, canvas)
+//controls.enableDamping = true
+
+camera.position.set(0,0,camerakhoangcach);
+
+camera.lookAt(0,0,0);
+scene.add(camera);
+controls.update()
+
+
 // Materials
-const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
+const verybasicmat = new THREE.MeshBasicMaterial( {
+	color: 0x000000,
+    transparent: true,
+    opacity: 0.5
+} );
+
+const sangbongmat = new THREE.MeshPhysicalMaterial({
+    color: 0xffffff, 
+    side: THREE.DoubleSide,
+    shadowSide: THREE.DoubleSide,
+    metalness: 0,
+    roughness: 0,
+    transparent: true,
+    opacity: 0.5,
+    depthWrite: false,
+    transmission: 1.0,
+    clearcoat: 1.0,
+    clearcoatRoughness: 0,
+})
 
 // Mesh
-var objectionxoay = [];
-var objectiondrag = [];
-//OBJLoad 
-objectload('static/untitled3_full.ply');
+
+// OBJLoad 
+let amb = {
+    //sound: objectload('static/sound container.ply', sangbongmat, true)
+};
+
+let anchor = [];
+
+// Mat Phang
+const planesz = 20;
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(planesz, planesz), new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.FrontSide}));
+const planewire = new THREE.Mesh(new THREE.PlaneGeometry(planesz, planesz), new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true}));
+const planegroup = new THREE.Group().add(plane).add(planewire);
+planegroup.rotateX(-Math.PI/2);
+planegroup.position.set(0,-1-frustum/2,0);
+
+scene.add(planegroup);
+
+
+// Anchor Container
+for (let dem = -2; dem <= soanchor - 3; dem++) {
+    anchor.push(wireframeload('static/hop-nhoo.ply', 'static/hop-to-nap.ply', verybasicmat, false, dem*4,-.5,0))
+}
+
+let boundinganchor = new THREE.Box3().setFromObject(anchortong);
+anchortong.position.set(0,-frustum/2,0);
+scene.add(anchortong);
+
+
 
 //// Light 2
 
@@ -58,13 +129,13 @@ pointLight2.position.set(0.85,0.48,0.48);
 //light2.addColor(light2color, 'color').onChange(() => {
 //    pointLight2.color.set(light2color.color);
 //}) 
-//// Light 3
 
+//// Ambient Light
 const ambient = new THREE.AmbientLight( 0xffffff, .3 );
 scene.add( ambient );
-const color = 0xFFFFFF;
-const intensity = 30;
-const light = new THREE.DirectionalLight(color, intensity);
+
+//// DirLight1
+const light = new THREE.DirectionalLight(0xFFFFFF, 30);
 light.position.set(-1, 2, 4);
 scene.add(light);
 
@@ -74,10 +145,6 @@ scene.add(light);
 /**
  * Sizes
  */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
 
 window.addEventListener('resize', () =>
 {
@@ -98,23 +165,6 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
-let aspect = sizes.width / sizes.height;
-let frustum = 3;
-const camera = new THREE.OrthographicCamera(-aspect * frustum, aspect * frustum, frustum, -frustum, 0.1, 1000)
-
-// Controls Orbit
-const controls = new OrbitControls(camera, canvas)
-//controls.enableDamping = true
-
-camera.position.set(0,0,10);
-
-camera.lookAt(0,0,0);
-scene.add(camera);
-controls.update()
 
 /**
  * Renderer
@@ -169,7 +219,8 @@ const tick = () =>
 
 /////////////////////////////////////////////////////////// DRAG
 /**/ 
-const controldrag = new DragControls(objectiondrag, camera, canvas );
+const controldrag = new DragControls(objectiondrag, camera, canvas);
+//controldrag.transformGroup = true;
 
 
 
@@ -187,32 +238,25 @@ const controldrag = new DragControls(objectiondrag, camera, canvas );
 // add event listener to highlight dragged objects
 
 controldrag.addEventListener( 'dragstart', function ( event ) {
-
-	event.object.material.color.set( 0xaaaaaa );
+    
+	event.object.material.transparent = false;
     controls.enabled = false;
 
 } );
 
 controldrag.addEventListener( 'dragend', function ( event ) {
 
-	event.object.material.color.set( 0xffffff );
+	event.object.material.transparent = true;
     controls.enabled = true;
+
 
 } );
 
 /**
  * OBJLoad
  */
-function objectload(link, link2) {
+function objectload(link, vatlieu, xoayornot) {
     let loader = new PLYLoader()
-    /*
-    let mtlloader = new MTLLoader();
-
-    mtlloader.load(mat, (mtl) => {
-        mtl.preload();
-        loader.setMaterials(mtl);
-    })
-    */
 
     loader.load(link, (objfile) => {
         //let objmesh = new THREE.Mesh(objfile, material);
@@ -233,38 +277,94 @@ function objectload(link, link2) {
                 //});
             //}
         //})
+        //scene.add(objfile);
+        //console.log(objectiondrag.length);
+
         objfile.computeVertexNormals();
 
-        let meshobj = new THREE.Mesh(objfile, new THREE.MeshPhysicalMaterial({
-                    color: 0xffffff, 
-                    side: THREE.DoubleSide,
-                    shadowSide: THREE.DoubleSide,
-                    metalness: 0,
-                    roughness: 0,
-                    transparent: true,
-                    opacity: 0.5,
-                    depthWrite: false,
-                    transmission: 1.0,
-                    clearcoat: 1.0,
-                    clearcoatRoughness: 0,
-        }))
+        let meshobj = new THREE.Mesh(objfile, vatlieu);
 
         meshobj.position.set(0,-.5,0);
 
         scene.add(meshobj);
+        if (xoayornot) { objectionxoay.push(meshobj); }
 
-        //scene.add(objfile);
         objectiondrag.push(meshobj);
-        objectionxoay.push(meshobj);
-        //console.log(objectiondrag.length);
+
+        return meshobj;
     });
-    
-    
-    
-   
 }
 
 
+function wireframeload(linkhop, linknap, vatlieu, xoayornot, diemx, diemy, diemz) {
+    let loader = new PLYLoader()
+    const matwire = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+    let meshtrunggian = new THREE.Object3D();
+    let meshcontainer = new THREE.Object3D();
+    let meshobjhop;
+    let meshobjwirehop;
+    let meshobjnap;
+    let meshobjwirenap;
+
+    loader.load(linknap, (objfile) => {
+        objfile.computeVertexNormals();
+        meshobjnap = new THREE.Mesh(objfile, matwire);
+        meshobjwirenap = new THREE.Mesh(objfile, vatlieu);
+
+        //meshobjnap.position.y = 1;
+        meshobjwirenap.position.y = .3;
+
+        meshobjwirenap.add(meshobjnap);
+        meshtrunggian.add(meshobjwirenap);
+
+
+        //meshcontainer.add(meshgroupnap);
+
+    });
+
+    loader.load(linkhop, (objfile) => {
+        objfile.computeVertexNormals();
+        meshobjhop = new THREE.Mesh(objfile, matwire);
+        meshobjwirehop = new THREE.Mesh(objfile, vatlieu);
+
+        meshobjhop.add(meshtrunggian);
+        meshobjwirehop.add(meshobjhop);
+        meshcontainer.add(meshobjwirehop);
+
+        //meshcontainer.add(meshgrouphop);
+
+    });
+
+
+    meshcontainer.position.set(diemx,diemy,diemz);
+
+    if (xoayornot) { objectionxoay.push(meshcontainer); }
+    objectiondrag.push(meshcontainer);
+
+    anchortong.add(meshcontainer);
+
+    return meshcontainer;
+}
+/**
+ * Change View Mode
+ */
+document.querySelector('#rotx').addEventListener('click', function(){
+    camera.position.set(camerakhoangcach, 0, 0);
+    camera.updateProjectionMatrix();
+    controls.update()
+})
+
+document.querySelector('#roty').addEventListener('click', function(){
+    camera.position.set(0, -camerakhoangcach, 0);
+    camera.updateProjectionMatrix();
+    controls.update()
+})
+
+document.querySelector('#rotz').addEventListener('click', function(){
+    camera.position.set(0, 0, camerakhoangcach);
+    camera.updateProjectionMatrix();
+    controls.update()
+})
 
 /**/
 tick()
