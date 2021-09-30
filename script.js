ea7edaaa2af36fb8c9d616801d692693933b5f96
vanghoa <baoanh1buinguyen@gmail.com>
2021-09-30 16:09:23 +0700

    UPDATE LAST

--- script.js
+++ script.js
@@ -3,20 +3,27 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.132.2/build/three.module
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js';
 import { DragControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/DragControls.js';
 import { PLYLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/PLYLoader.js';
+import { LineMaterial } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/lines/LineMaterial.js';
+import { ConvexGeometry } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/geometries/ConvexGeometry.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
+/*import { Line2 } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/lines/Line2.js';
+import { LineMaterial } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/lines/LineMaterial.js';
+import { LineGeometry } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/lines/LineGeometry.js';*/
 //import * as dat from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/libs/dat.gui.module.js';
 
 let objectionxoay = [];
 let objectiondrag = [];
 let soanchor = 3;
-//
+// Anchor
 let anchortong = new THREE.Object3D();
 let objectiondragcontainer = [];
 let controlarray = [];
 let anchposition = [];
-let anchor = [];
-//
+let anchcolor = [];
 let vmhhelpercheck = false;
+// Data drag
+let datadragable =[];
+let controlarraydata = [];
 
 const sizes = {
     width: window.innerWidth,
@@ -32,8 +39,33 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+const texture = new THREE.TextureLoader().load('static/Street View 3602 1.jpg');
+
+const texturemat = new THREE.MeshBasicMaterial({map: texture});
+
+const geoenv = new THREE.SphereGeometry(30,60,40);
+
+geoenv.scale(-1,1,1);
+
+const meshenv = new THREE.Mesh(geoenv, texturemat);
+meshenv.rotation.set(0,0,0);
+
+scene.add(meshenv)
+
+/*
+scene.background = new THREE.CubeTextureLoader()
+	.setPath( 'static/textures/' )
+	.load( [
+		'px.png',
+		'nx.png',
+		'py.png',
+		'ny.png',
+		'pz.png',
+		'nz.png'
+	] );
+*/
 //environment
-scene.background = new THREE.Color(0x999999);
+//scene.background = new THREE.Color(0x999999);
 
 /**
  * Camera
@@ -62,6 +94,12 @@ const verybasicmat = new THREE.MeshBasicMaterial( {
     opacity: .8
 } );
 
+const vmhmatsurface = new THREE.MeshBasicMaterial( {
+	color: 0xff0000,
+    transparent: true,
+    opacity: .3
+} );
+
 const wireframemat = new THREE.MeshBasicMaterial({
     color: 0x000000, 
     wireframe: true,
@@ -73,10 +111,21 @@ const wireframematgeo = new THREE.LineBasicMaterial({
     opacity: .3,
 })
 
-const vmhlinemat = new THREE.LineDashedMaterial({
+/*
+const vmhlinemat = new LineMaterial({
     color: 0x0026fc,
     dashSize: .3,
-    gapSize: 1
+    gapSize: 1,
+    linewidth: .003,
+    dashed: true,
+    //vertexColors: true,
+    //alphaToCoverage: true,
+}) 
+*/
+const vmhlinematold = new THREE.LineDashedMaterial({
+    color: 0x0026fc,
+    dashSize: .3,
+    gapSize: 1,
 })
 
 const sangbongmat = new THREE.MeshPhysicalMaterial({
@@ -92,6 +141,7 @@ const sangbongmat = new THREE.MeshPhysicalMaterial({
     transmission: 1.0,
     clearcoat: 1.0,
     clearcoatRoughness: 0,
+    envMap: texture
 })
 
 // Mesh
@@ -117,9 +167,9 @@ planegroup.position.set(0,-frustum/2,0);
 scene.add(planegroup);
 
 //Data
-let data = [
-    objectload('static/Tree1.ply', sangbongmat, true, planeszheight/2, planegroup.position.y, -planeszwidth/2, .75, .75, .75)
-];
+
+//objectload('static/Tree1.ply', sangbongmat, true, true, planeszheight/2, planegroup.position.y, -planeszwidth/2, .75, .75, .75, 1);
+
 
 
 // Anchor Container
@@ -141,24 +191,24 @@ function draganchor(n, die) {
 }
 
 function generateanchor(soanchorx) {
+            controlarray.forEach((ele) => {
+                ele.dispose();
+            })
 
-            console.log(anchortong.children);
             for (let i = 0; i <= (anchortong.children.length - 1); i++) {
                 anchortong.children[i].remove(anchortong.children[i].children[0])
-                //anchortong.remove(anchortong.children[i]);
             }
 
             scene.remove(anchortong);
             objectiondragcontainer = [];
             controlarray = [];
             anchposition = [];
-            anchor = [];
-            console.log('okla');
+            //anchcolor = [];
 
             ///////////////////
 
             for (let dem = -Math.floor(soanchorx/2), colordem = 0, nameanc = 0; dem <= soanchorx -1 -Math.floor(soanchorx/2); dem++, colordem+=.2, nameanc++) {
-                anchor.push(wireframeload('static/hop-nhoo.ply', 'static/hop-to-nap.ply', false, dem*2.3,0,0, colordem, nameanc))
+                wireframeload('static/hop-nhoo.ply', 'static/hop-to-nap.ply', false, dem*2.3,0,0, colordem, nameanc);
             }
             ////////// FUCK2
             for (let k = 0; k <= controlarray.length - 1; k++) 
@@ -172,12 +222,19 @@ function generateanchor(soanchorx) {
                     }
                     controls.enabled = false;
                     controldrag.enabled = false;
+                    for (let s = 0; s <= controlarraydata.length - 1; s++){
+                            controlarraydata[s].enabled = false;
+                    }
                 });
             
                 controlarray[k].addEventListener('drag', function(event){
                     let tramcamqua = new THREE.Vector3();
                     event.object.getWorldPosition(tramcamqua);
                     anchposition[k] = tramcamqua;
+                    /*
+                    anchcolor[k] = event.object.material.color.r;
+                    anchcolor[(k+1)] = event.object.material.color.g;
+                    anchcolor[(k+2)] = event.object.material.color.b;*/
                 }); 
             
                 controlarray[k].addEventListener('dragend', function(event){
@@ -193,6 +250,15 @@ function generateanchor(soanchorx) {
                     let tramcamqua = new THREE.Vector3();
                     event.object.getWorldPosition(tramcamqua);
                     anchposition[k] = tramcamqua;
+
+                    for (let s = 0; s <= controlarraydata.length - 1; s++){
+                            controlarraydata[s].enabled = true;
+                    }
+                    /*
+                    anchcolor[k] = event.object.material.color.r;
+                    anchcolor[(k+1)] = event.object.material.color.g;
+                    anchcolor[(k+2)] = event.object.material.color.b;*/
+
 
                     let tramcamqua2 = new THREE.Vector3();
                     objectiondragcontainer[k][0].getWorldPosition(tramcamqua2);
@@ -247,23 +313,45 @@ vmhxoa.addEventListener('click', function(){
 
 // Line VMH
 let pointline;
+let pointmesh;
 
 
 function linevmh() {
     scene.remove(pointline);
+    scene.remove(pointmesh);
     let pointgraph = [];
+    //let colorgraph = [];
 
     for (let v = 0; v <= (anchposition.length - 2); v++) {
         for (let t = v + 1; t <= (anchposition.length - 1); t++) {
             pointgraph.push(anchposition[v], anchposition[t]);
+/*
+            pointgraph.push(anchposition[v].x, anchposition[v].y, anchposition[v].z, anchposition[t].x, anchposition[t].y, anchposition[t].z);
+            colorgraph.push(anchcolor[v], anchcolor[v+1], anchcolor[v+2], anchcolor[t], anchcolor[t+1], anchcolor[t+2]);
+            */
         }
     }
-
+/*
+    const pointmetry = new LineGeometry();
+    pointmetry.setPositions(pointgraph);
+    pointmetry.setColors(colorgraph);
+          pointline = new Line2(pointmetry, vmhlinemat); */
+          
+    if (anchposition.length >= 7) {
+    const pointsurface = new ConvexGeometry(anchposition);
+    pointmesh = new THREE.Mesh(pointsurface, vmhmatsurface);
+    scene.add(pointmesh);
+    }
+    
     const pointmetry = new THREE.BufferGeometry().setFromPoints(pointgraph);
-          pointline = new THREE.Line(pointmetry, vmhlinemat);
-          pointline.computeLineDistances();
+    pointline = new THREE.Line(pointmetry, vmhlinematold);
+    
+    pointline.computeLineDistances();
     scene.add(pointline);
 }
+
+// Attribute
+//const atb1 = objectload('static/hop-to-nap.ply', verybasicmat, false, true, 0, 0, 0, .5, .5, .5, 2)
 
 
 //// Light 2
@@ -295,8 +383,8 @@ const ambient = new THREE.AmbientLight( 0xffffff, .3 );
 scene.add( ambient );
 
 //// DirLight1
-const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-light.position.set(-1, 2, 4);
+const light = new THREE.DirectionalLight(0xFFFFFF, 3);
+light.position.set(-1, 2, .1);
 scene.add(light);
 
 
@@ -336,7 +424,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.setClearColor(0xe5e5e5);
+//renderer.setClearColor(0xe5e5e5);
 
 
 
@@ -442,6 +530,10 @@ controldrag.addEventListener( 'dragstart', function ( event ) {
     for (let s = 0; s <= controlarray.length - 1; s++){
             controlarray[s].enabled = false;
     }
+    
+    for (let s = 0; s <= controlarraydata.length - 1; s++){
+        controlarraydata[s].enabled = false;
+}
 } );
 
 controldrag.addEventListener( 'dragend', function ( event ) {
@@ -451,11 +543,15 @@ controldrag.addEventListener( 'dragend', function ( event ) {
     for (let s = 0; s <= controlarray.length - 1; s++){
             controlarray[s].enabled = true;
     }
+    
+    for (let s = 0; s <= controlarraydata.length - 1; s++){
+        controlarraydata[s].enabled = true;
+}
 } );
 /**
  * OBJLoad
  */
-function objectload(link, vatlieu, xoayornot, vtx, vty, vtz, scl1, scl2, scl3) {
+function objectload(link, vatlieu, xoayornot, dragornot, vtx, vty, vtz, scl1, scl2, scl3, namedata) {
     let loader = new PLYLoader()
 
     loader.load(link, (objfile) => {
@@ -487,12 +583,11 @@ function objectload(link, vatlieu, xoayornot, vtx, vty, vtz, scl1, scl2, scl3) {
         meshobj.scale.set(scl1, scl2, scl3);
         meshobj.position.set(vtx,vty,vtz);
 
+        meshobj.name = String(namedata);
         scene.add(meshobj);
         if (xoayornot) { objectionxoay.push(meshobj); }
 
-        objectiondrag.push(meshobj);
-
-        return meshobj;
+        if (dragornot) { objectiondrag.push(meshobj); }
     });
 }
 
@@ -515,6 +610,7 @@ function wireframeload(linkhop, linknap, xoayornot, diemx, diemy, diemz, hslhue,
     } );
     vatlieuhop.color.setHSL(hslhue,1,.5);
     vatlieunap.color.setHSL(hslhue,1,.3);
+    anchcolor.push(vatlieuhop.color.r, vatlieuhop.color.g, vatlieuhop.color.b);
 
 
     loader.load(linknap, (objfile) => {
@@ -559,15 +655,54 @@ function wireframeload(linkhop, linknap, xoayornot, diemx, diemy, diemz, hslhue,
 
     draganchor(nameanchor, meshcontainer);
 
-    return meshcontainer;
+    //return meshcontainer;
 }
 
-GLTFloaddata();
+GLTFloaddata('static/tree.glb', 2.5, 2.5, 2.5, planeszheight/2, planegroup.position.y-1.5, -planeszwidth/2, 1, true, true, 0);
 
-function GLTFloaddata() {
+function GLTFloaddata(link, sclx, scly, sclz, vtx, vty, vtz, identity, xoayornot, dragornot, thutu) {
     const loader = new GLTFLoader();
-    loader.load('static/Tree.glb', function (gltf) {
+    loader.load( link , function (gltf) {
+        gltf.scene.scale.set(sclx,scly,sclz);
+        gltf.scene.position.set(vtx,vty,vtz);
+        gltf.scene.name = String(identity);
         scene.add(gltf.scene);
+        if (xoayornot) { objectionxoay.push(gltf.scene); }
+
+        if (dragornot) { 
+            datadragable.push([gltf.scene]);
+            controlarraydata.push(new DragControls(datadragable[thutu], camera, canvas));
+            controlarraydata[thutu].transformGroup = true;
+            controlarraydata[thutu].addEventListener( 'dragstart', function ( event ) {
+                controldrag.enabled = false;
+                //event.object.material.transparent = false;
+                controls.enabled = false;
+                for (let s = 0; s <= controlarray.length - 1; s++){
+                        controlarray[s].enabled = false;
+                }
+                for (let s = 0; s <= controlarraydata.length - 1; s++){
+                    if (s != thutu) {
+                        controlarraydata[s].enabled = false;
+                    }
+                }
+            } );
+            
+            controlarraydata[thutu].addEventListener( 'dragend', function ( event ) {
+                controldrag.enabled = false;
+                //event.object.material.transparent = true;
+                controls.enabled = true;
+                for (let s = 0; s <= controlarray.length - 1; s++){
+                        controlarray[s].enabled = true;
+                }
+                for (let s = 0; s <= controlarraydata.length - 1; s++){
+                    if (s != thutu) {
+                        controlarraydata[s].enabled = true;
+                    }
+                }
+            } );
+
+         }
+
     })
 }
 
@@ -602,4 +737,97 @@ function numbercontainer() {
 }
 
 /**/
+//console.log(anchortong);
+
+document.addEventListener('keydown', function (e) {
+    if (e.code === "KeyB") {  
+        chucnangcut();
+    } else if (e.code === "KeyN") {
+
+    }
+});
+
+
+function chucnangcut() {
+    for (let i = 0; i <= (scene.children.length - 1); i++) {
+        if (scene.children[i].name === "1") {
+            scene.children[i].visible = !scene.children[i].visible;
+            //scene.children[i].traverse((child) => {
+                //if (child.isMesh) {
+                    //child.material.visible = false;
+                //}
+            //});
+        }
+    }
+}
+
+
+
+/////////////////////////////////////////////
+document.querySelector('#xoamoithu').addEventListener('click', function(){disposeHierarchy(scene, disposeNode)});
+console.log(scene);
 tick()
+
+function disposeNode (node)
+{
+    if (node instanceof THREE.Mesh)
+    {
+        if (node.geometry)
+        {
+            node.geometry.dispose ();
+        }
+
+        if (node.material)
+        {
+            if (node.material instanceof THREE.MeshFaceMaterial)
+            {
+                $.each (node.material.materials, function (idx, mtrl)
+                {
+                    if (mtrl.map)               mtrl.map.dispose ();
+                    if (mtrl.lightMap)          mtrl.lightMap.dispose ();
+                    if (mtrl.bumpMap)           mtrl.bumpMap.dispose ();
+                    if (mtrl.normalMap)         mtrl.normalMap.dispose ();
+                    if (mtrl.specularMap)       mtrl.specularMap.dispose ();
+                    if (mtrl.envMap)            mtrl.envMap.dispose ();
+                    if (mtrl.alphaMap)          mtrl.alphaMap.dispose();
+                    if (mtrl.aoMap)             mtrl.aoMap.dispose();
+                    if (mtrl.displacementMap)   mtrl.displacementMap.dispose();
+                    if (mtrl.emissiveMap)       mtrl.emissiveMap.dispose();
+                    if (mtrl.gradientMap)       mtrl.gradientMap.dispose();
+                    if (mtrl.metalnessMap)      mtrl.metalnessMap.dispose();
+                    if (mtrl.roughnessMap)      mtrl.roughnessMap.dispose();
+
+                    mtrl.dispose ();    // disposes any programs associated with the material
+                });
+            }
+            else
+            {
+                if (node.material.map)              node.material.map.dispose ();
+                if (node.material.lightMap)         node.material.lightMap.dispose ();
+                if (node.material.bumpMap)          node.material.bumpMap.dispose ();
+                if (node.material.normalMap)        node.material.normalMap.dispose ();
+                if (node.material.specularMap)      node.material.specularMap.dispose ();
+                if (node.material.envMap)           node.material.envMap.dispose ();
+                if (node.material.alphaMap)         node.material.alphaMap.dispose();
+                if (node.material.aoMap)            node.material.aoMap.dispose();
+                if (node.material.displacementMap)  node.material.displacementMap.dispose();
+                if (node.material.emissiveMap)      node.material.emissiveMap.dispose();
+                if (node.material.gradientMap)      node.material.gradientMap.dispose();
+                if (node.material.metalnessMap)     node.material.metalnessMap.dispose();
+                if (node.material.roughnessMap)     node.material.roughnessMap.dispose();
+
+                node.material.dispose ();   // disposes any programs associated with the material
+            }
+        }
+    }
+}   // disposeNode
+
+function disposeHierarchy (node, callback)
+{
+    for (var i = node.children.length - 1; i >= 0; i--)
+    {
+        var child = node.children[i];
+        disposeHierarchy (child, callback);
+        callback (child);
+    }
+}
